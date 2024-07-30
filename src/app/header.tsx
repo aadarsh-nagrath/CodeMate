86fc4f13a627e9e2b8916697567886e94cc08448
aadarsh-nagrath <anagrath1@gmail.com>
2024-07-31 04:10:35 +0530

    next-auth & google Oauth setup | Header started| Provider fix | Drizzele adaptor added | Schema changed a bit fixed errors resolved|  google client id and secret env setup|
    Resource -  https://authjs.dev/getting-started/adapters/drizzle
    https://next-auth.js.org/getting-started/example

---
+++ src/app/header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "../components/ui/button";
+import { ModeToggle } from "../components/mode-toggle";
+
+export function Header(){
+    const session = useSession();
+    return(
+        <header>
+            <div>
+                {session.data ? (
+                    <Button onClick={() => signOut()}>Sign Out</Button>
+                ):(
+                    <Button onClick={() => signIn("google")}>Sign In</Button>
+                )}
+                <ModeToggle/>
+
+                {session.data?.user?.name}
+            </div>
+        </header>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from "./../components/theme-provider"
 import type { Metadata } from "next";
+import { ModeToggle } from "./../components/mode-toggle";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ModeToggle } from "./../components/mode-toggle";
+import Provider from "./providers";
+import { Header } from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,15 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-          <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div><ModeToggle/></div>
+      <Provider>
+          <Header/>
           {children}
-        </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );
